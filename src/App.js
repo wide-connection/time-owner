082f8dfe64161f5b9502a980ec6934294c9e9cf0
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-02 21:08:24 -0500

    Added Round buttons at bottom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import './App.css';
 import { HealthBtn } from './components/classesLogginButtons/itemButtons/HealthBtn';
 import { WorkBtn } from './components/classesLogginButtons/itemButtons/WorkBtn';
@@ -13,33 +13,39 @@ import Footer from './components/Footer/Footer';
 import DayTable from './components/Tables/DayTable'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-function App() {
+
+
+const App = () => {
   return (
-
     <div className="page-container">
-    <div className="content-wrap">
-    <Header/>
+      <div className="content-wrap">
+        <Header />
 
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', padding: 15}}>
-    <WorkBtn>Work</WorkBtn>
-    <HealthBtn>Health</HealthBtn>
-    <HobbyBtn>Hobby</HobbyBtn>
-    <FamilyBtn>Family</FamilyBtn>
-    <RoutineBtn>Routine</RoutineBtn>
-    <FriendBtn>Friends</FriendBtn>
-    <EducationBtn>Education</EducationBtn>
-    <EntertainmentBtn>Entertainment</EntertainmentBtn>
+        <DayTable>
+
+        </DayTable>
+
+        <div className="buttons" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 15 }}>
+          {/* <WorkBtn>Work</WorkBtn>
+          <HealthBtn>Health</HealthBtn>
+          <HobbyBtn>Hobby</HobbyBtn>
+          <FamilyBtn>Family</FamilyBtn>
+          <RoutineBtn>Routine</RoutineBtn>
+          <FriendBtn>Friends</FriendBtn>
+          <EducationBtn>Education</EducationBtn>
+          <EntertainmentBtn>Entertainment</EntertainmentBtn> */}
+          <WorkBtn /> <span>Work</span> 
+          <HealthBtn /> <span>Health</span>
+          <HobbyBtn /> <span>Hobby</span>
+          <FamilyBtn /> <span>Family</span>
+          <RoutineBtn /> <span>Routine</span>
+          <FriendBtn /> <span>Friends</span>
+          <EducationBtn /> <span>Education</span>
+          <EntertainmentBtn /> <span>Entertainment</span>
+        </div>
+      </div>
+      <Footer />
     </div>
-    
-    <DayTable>
-
-    </DayTable>
-  
-    </div>
-    <Footer/>
-    </div>
-
-
   );
 }
 
