dde43b0f4cae351bb7d4addc5864b9b94a7fe025
HyunSeongLee <gustjd2680@gmail.com>
2021-03-04 01:00:10 +0900

    spinner updated

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import RingLoader from "react-spinners/RingLoader";
+import ClockLoader from "react-spinners/ClockLoader";
 import logo from '../Images/TimeOwner.png'
 
 const override = `
@@ -22,13 +22,13 @@ const Loading = () => {
             style={{
                     width: "400px",
                     position: "absolute",
-                    top: "35%",
-                    left: "36%"
+                    top: "31%",
+                    left: "34%"
                   }}/>
-            <RingLoader
+            <ClockLoader
             color={"#4A90E2"} 
             css={override}      
-            size={125} 
+            size={100} 
             /> 
         </div>
     )
