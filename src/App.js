2620e359af8f76815a912f3ce948983108a6f965
HyunSeongLee <gustjd2680@gmail.com>
2021-03-03 14:32:05 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React from 'react'; 
 import './App.css';
-import { HealthBtn } from './components/classesLogginButtons/itemButtons/HealthBtn';
-import { WorkBtn } from './components/classesLogginButtons/itemButtons/WorkBtn';
-import { HobbyBtn } from './components/classesLogginButtons/itemButtons/HobbyBtn'
-import { FamilyBtn } from './components/classesLogginButtons/itemButtons/FamilyBtn'
-import { RoutineBtn } from './components/classesLogginButtons/itemButtons/RoutineBtn'
-import { FriendBtn } from './components/classesLogginButtons/itemButtons/FriendBtn'
-import { EducationBtn } from './components/classesLogginButtons/itemButtons/EducationBtn'
-import { EntertainmentBtn } from './components/classesLogginButtons/itemButtons/EntertainmentBtn'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer';
-import DayTable from './components/Tables/DayTable'
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
 
     <div className="page-container">
     <div className="content-wrap">
     <Header/>
-
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', padding: 15}}>
-    <WorkBtn>Work</WorkBtn>
-    <HealthBtn>Health</HealthBtn>
-    <HobbyBtn>Hobby</HobbyBtn>
-    <FamilyBtn>Family</FamilyBtn>
-    <RoutineBtn>Routine</RoutineBtn>
-    <FriendBtn>Friends</FriendBtn>
-    <EducationBtn>Education</EducationBtn>
-    <EntertainmentBtn>Entertainment</EntertainmentBtn>
-    </div>
-    
-    <DayTable>
-
-    </DayTable>
+   
+   
   
     </div>
     <Footer/>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react'; 
-import './Header.css'; 
-import {Navbar, Nav} from 'react-bootstrap';
+import React, { Component } from 'react'
+import './Header.css'
 
-class Header extends Component {
-    render() {
-        return (
+class Header extends Component {  
+  
+    render() {      
+        return (                    
           <div className="App">
             <nav className="navbar">
-
               <div className="navbar_logo">
-              <i class="far fa-calendar fa -alt"></i>
-              <a href="">Chocolander</a>
+                <i class="fas fa-calendar-week"></i> &nbsp;
+              <a href="/#">Chocolander</a>
               </div>
 
-              <div className="navbar_menu">
+              <ul className="navbar_menu">
+                  <li><a href="/#">Home</a></li>
+                  <li><a href="/#">Login</a></li>
+                  <li><a href="/#">About Us</a></li>
+                  <li><a href="/#">Contact</a></li>                  
+              </ul>
 
+              <div className="navbar_icons">                
+                 <li><i class="fab fa-github"></i></li> 
+                 <li><i class="fab fa-twitter"></i></li>
               </div>
 
-              <div className="navbar_links">
-
-              </div>
+              <a href="/#" className="navbar_toogleBtn">
+                  <i class="fas fa-bars"></i>
+              </a>
 
             </nav>
+         
           </div>
+ 
         );
     }
 }
