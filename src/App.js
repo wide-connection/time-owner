6fe44a114c188f971b20f0a8d48e8db73a9befda
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-03 22:34:10 -0500

    Added draggable buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Loading from './components/Loading/Loading'
+import Calender from './components/calender/Calender';
+import Categories from './components/Categories/Categories';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true); 
-    useEffect(() => {
-      setTimeout(() => {
-        setIsLoading(false); 
-      }, 3000); 
-    })
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  })
   return (
 
     <div className="App">
-     {
-       isLoading ?
-       <Loading/>
-       :
-       <div>
-         <Header/>
-         <Footer/>
-       </div>
-     }
+      {
+        isLoading ?
+          <Loading />
+          :
+          <div>
+            <Header />
+            <Calender />
+            <div className='buttons-container'>
+              <Categories className="Work-btn" color='deepSkyBlue' content='Work' />
+              <Categories className="Family-btn" color='yellow' content='Family' />
+              <Categories className="Education-btn" color='mediumSpringGreen' content='Education' />
+              <Categories className="Entertainment-btn" color='lightCoral' content='Entertainment' />
+              <Categories className="Routine-btn" color='lightGray' content='Routine' />
+              <Categories className="Friends-btn" color='pink' content='Friends' />
+              <Categories className="Health-btn" color='gold' content='Health' />
+              <Categories className="Hobby-btn" color='plum' content='Hobby' />
+            </div>
+            <Footer />
+          </div>
+      }
 
     </div>
 
---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './Categories.css';
+import Draggable from "react-draggable";
+
+const Categories = (props) => {
+    const [color] = useState(props.color);
+    const [content] = useState(props.content);
+    const [className] = useState(props.className);
+
+    return (
+        <Draggable>
+            <div className={className}>
+                <button style={{ backgroundColor: color }}></button>
+                <span>{content}</span>
+            </div>
+        </Draggable>
+    )
+}
+
+export default Categories;
