45b87de2a3793ba2c866cb9f55625812ccdec8b5
HyunSeongLee <gustjd2680@gmail.com>
2021-03-05 13:18:02 +0900

    creating table

--- src/categories/calendarTask.js
+++ src/categories/calendarTask.js
@@ -15,22 +15,22 @@ export const getCategoryCollection = ()=> ([
 ])
 
 export function insertNewTask(data) {
-    let tasks=getAllTasks();
+    let tasks = getAllTasks();
     data['id'] = generateTaskId();
-    tasks.push(data)
-    localStorage.setItem(KEYS.tasks, JSON.stringify(tasks))
+    tasks.push(data); 
+    localStorage.setItem(KEYS.tasks, JSON.stringify(data))
 }
 
 export function generateTaskId() {
     if (localStorage.getItem(KEYS.taskId) == null)
-        localStorage.getItem(KEYS.taskId, '0')
+        localStorage.setItem(KEYS.taskId, '0')
     var id = parseInt(localStorage.getItem(KEYS.taskId))
     localStorage.setItem(KEYS.taskId, (++id).toString())
-    return id; 
+    return id;
 }
 
 export function getAllTasks() {
-    if(localStorage.getItem(KEYS.tasks) === null)
+    if (localStorage.getItem(KEYS.tasks) == null)
         localStorage.setItem(KEYS.tasks, JSON.stringify([]))
-    return JSON.parse(localStorage.getItem(KEYS.tasks))
+    return JSON.parse(localStorage.getItem(KEYS.tasks));
 }
--- src/components/TableForm/TableForms.js
+++ src/components/TableForm/TableForms.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TableForm from './TableForm'
-import { Paper, makeStyles } from '@material-ui/core'
+import { Paper, makeStyles, TableBody, TableRow, TableCell } from '@material-ui/core'
+import useTable from "../../components/useTable"
+import * as calendarTask from '../../categories/calendarTask'
 
 const useStyles = makeStyles(theme =>({
     pageContent: {
@@ -12,10 +14,28 @@ const useStyles = makeStyles(theme =>({
 export default function TableForms() {
 
     const classes = useStyles();
+    const [records, setRecords]=useState(calendarTask.getAllTasks()); 
+
+    const {
+        TblContainer
+    }=useTable();
 
     return (
         <Paper className={classes.pageContent}>
-            <TableForm/>
+            {/* <TableForm/> */}
+            <TblContainer>
+                <TableBody>
+                    {
+                        records.map(item =>
+                            (<TableRow key={item.id}>
+                                <TableCell>{item.task}</TableCell>
+                                <TableCell>{item.length}</TableCell>
+                                <TableCell>{item.category}</TableCell>
+                                <TableCell>{item.markComplete}</TableCell>
+                            </TableRow>))                            
+                    }
+                </TableBody>
+            </TblContainer>
         </Paper>
         
     )
