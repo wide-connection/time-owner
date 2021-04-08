c98111006c9bfd19ac400ac06426bfa5d7066775
Jung Soo Choi <chlwjdtn15@hotmail.com>
2021-04-08 17:21:31 +0900

    Changed 'Our Future' background colour
    Changed 'Our Future' background colour

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Statistics.css';
+
+const Statistics = () => {
+
+
+
+
+    return (
+        <h1>hi</h1>
+    );
+}
+
+
+export default Statistics;
