32f8560b9bb539bd4230f2b12a1cc56685186c7c
Jung Soo Choi <chlwjdtn15@hotmail.com>
2021-04-10 00:23:58 +0900

    Added bar charts
    Added bar charts

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Statistics.css';
-import { Pie, defaults } from 'react-chartjs-2'
+import { Bar, Pie, defaults } from 'react-chartjs-2'
 
 
 defaults.global.legend.labels.boxWidth = 100;
@@ -12,72 +12,198 @@ const Statistics = () => {
     
 
             <div>
-                <h1>Statistics</h1>
-                <Pie
-                data = {{
-                    labels: ['Sleep', 'Work', 'Hobby'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3],
+           
+
+                        <div class = "pieCharts">
+
+                            <div class = "dailyPie">
+                                            <h1>Daily Statistics</h1>
+                                        <Pie
+                                            data = {{
+                                            labels: ['Sleep', 'Work', 'Hobby'],
+                                            datasets: [{
+                                                label: '# of Votes',
+                                                data: [12, 19, 3],
+
+
+                                                backgroundColor: [
+                                                    'rgb(255, 99, 132)',
+                                                    'rgb(54, 162, 235)',
+                                                    'rgb(255, 205, 86)'
+                                                    ],
+                                                    hoverOffset: 4
+
+                                            }]
+                                        }}
+                                        height = {200}
+                                        width = {100}
+                                        options = {{
+                                            maintainAspectRatio: false,
+
+
+                                            layout: {
+                                                padding: {
+                                                    top: 20,
+                                                    
+                                                }
+                                            }
+                                        }}
+
+                                        />
+  
+                             </div>
+
+                                    <div class = "dailyPie">
+                                    <h1>Weekly Statistics</h1>
+                                    <Pie
+                                    data = {{
+                                        labels: ['Sleep', 'Work', 'Hobby'],
+                                        datasets: [{
+                                            label: '# of Votes',
+                                            data: [12, 19, 3],
+
+
+                                            backgroundColor: [
+                                                'rgb(255, 99, 132)',
+                                                'rgb(54, 162, 235)',
+                                                'rgb(255, 205, 86)'
+                                                ],
+                                                hoverOffset: 4
+
+                                        }]
+                                    }}
+                                    height = {200}
+                                    width = {100}
+                                    options = {{
+                                        maintainAspectRatio: false,
+
+
+                                        layout: {
+                                            padding: {
+                                                top: 20
+                                            }
+                                        }
+                                    }}
+
+                                    />
+                                        
+                                </div>
+                 </div>
+                        
+                        <div class = "barCharts">
+                                <div class = "dailyPie">
+                                                    <h1>Daily Statistics</h1>
+                                                <Bar
+                                                    data = {{
+                                                    labels: ['Sleep', 'Work', 'Hobby'],
+                                                    datasets: [{
+                                                        label:'Your Daily Statistics in bar chart',
+                                                        data: [12, 19, 3],
 
 
 
-                        //this is for bar chart
-                        /*
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1,*/
+                                                        //this is for bar chart
+                                                        
+                                                        backgroundColor: [
+                                                            'rgba(255, 99, 132, 0.2)',
+                                                            'rgba(54, 162, 235, 0.2)',
+                                                            'rgba(255, 206, 86, 0.2)'
+                                                            
+                                                        ],
+                                                        borderColor: [
+                                                            'rgba(255, 99, 132, 1)',
+                                                            'rgba(54, 162, 235, 1)',
+                                                            'rgba(255, 206, 86, 1)'
+                                                          
+                                                        ],
+                                                        borderWidth: 1,
 
-                        backgroundColor: [
-                            'rgb(255, 99, 132)',
-                            'rgb(54, 162, 235)',
-                            'rgb(255, 205, 86)'
-                          ],
-                          hoverOffset: 4
+                                                    }]
+                                                }}
+                                                height = {200}
+                                                width = {100}
+                                                options = {{
+                                                    maintainAspectRatio: false,
 
-                    }]
-                }}
-                height = {500}
-                width = {100}
-                options = {{
-                    maintainAspectRatio: false,
+                                                    //this is for bar chart
+                                                    scales: {
+                                                        yAxes: [{
+                                                            ticks: {
+                                                            beginAtZero: true
+                                                                }
+                                                            
+                                                                }]
+                                                            
+                                                        },
 
-                    //this is for bar chart
-                    /*scales: {
-                        yAxes: [{
-                            ticks: {
-                            beginAtZero: true
-                             }
-                            
-                             }]
-                            
-                     },*/
+                                                    layout: {
+                                                        padding: {
+                                                            top: 20,
+                                                            
+                                                        }
+                                                    }
+                                                }}
 
-                    layout: {
-                        padding: {
-                            top: 20
-                        }
-                    }
-                }}
+                                                />
+        
+                                    </div>
 
-                />
+                                            <div class = "dailyPie">
+                                            <h1>Weekly Statistics</h1>
+                                            <Bar
+                                            data = {{
+                                                labels: ['Sleep', 'Work', 'Hobby'],
+                                                datasets: [{
+                                                    label: 'Your weekly Statistics in bar chart',
+                                                    data: [12, 19, 3],
 
 
 
+                                                    //this is for bar chart
+                                                    
+                                                    backgroundColor: [
+                                                        'rgba(255, 99, 132, 0.2)',
+                                                        'rgba(54, 162, 235, 0.2)',
+                                                        'rgba(255, 206, 86, 0.2)'
+                                                       
+                                                    ],
+                                                    borderColor: [
+                                                        'rgba(255, 99, 132, 1)',
+                                                        'rgba(54, 162, 235, 1)',
+                                                        'rgba(255, 206, 86, 1)'
+                                                    
+                                                    ],
+                                                    borderWidth: 1,
+
+                                                }]
+                                            }}
+                                            height = {200}
+                                            width = {100}
+                                            options = {{
+                                                maintainAspectRatio: false,
+
+                                                //this is for bar chart
+                                                scales: {
+                                                    yAxes: [{
+                                                        ticks: {
+                                                        beginAtZero: true
+                                                            }
+                                                        
+                                                            }]
+                                                        
+                                                    },
+
+                                                layout: {
+                                                    padding: {
+                                                        top: 20
+                                                    }
+                                                }
+                                            }}
+
+                                            />
+                            </div>
+                        </div>
+                
             </div>
         );
     }
