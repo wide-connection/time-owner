aa50eb4c2ac153c262be7f7e4be89a229335281b
yunseon lee <ylee111@myseneca.ca>
2021-04-10 23:36:00 -0400

    login constructor done and render structure

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import './Login.css';
+import { Form, Row, Col, Container, Button } from 'react-bootstrap';
 
-const Login = () => {
-  return <p>connection test</p>;
-};
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: {
+        userID: String,
+        password: String,
+      },
+      loggedIn: false,
+    };
+  }
 
+  render() {
+    // if (this.state.loggedIn) {
+    //   <p>render works</p>;
+    // }
+    // return(
+    //     <p>render works2</p>;
+    // );
+
+    return <h2 className="titleLogIn">Log In</h2>;
+  }
+}
 export default Login;
